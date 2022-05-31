51d86acd340b89da5333906c42b6fab64594c1ef
olyalew <99914485+olyalew@users.noreply.github.com>
2022-05-31 13:57:03 +0300

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,53 @@
+import requests
+import datetime
+from pprint import pprint
+from config import open_weather_token
+
+def get_weather(city, open_weather_token):
+
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
+
+    try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
+        )
+        data = r.json()
+        pprint(data)
+
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окошко"
+
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+
+        print(f"Погода в городе: {city}\nТемпература: {cur_weather}С {wd} \n"
+              f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind}\n"
+              f"Восход солнца: {sunrise_timestamp}\n"
+              f"Хорошего дня)"
+        )
+
+    except Exception as ex:
+        print(ex)
+        print("Проверьте название города")
+
+def main():
+    city = input("Enter city: ")
+    get_weather(city, open_weather_token)
+
+if __name__ == '__main__':
+    main()
